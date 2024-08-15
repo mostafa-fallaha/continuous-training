6e9bb84b63def824437b53e856e08a1e86291a2f
mostafa fallaha <moostafafallaha@gmail.com>
2024-08-15 09:39:16 +0300

    Add 2k

---
+++ model_training.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import dvc.api, dvc.repo
+import io
+from pathlib import Path
+from sklearn.linear_model import SGDRegressor, LinearRegression
+from sklearn.metrics import root_mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings('ignore')
+
+url = "https://github.com/mostafa-fallaha/continuous-training"
+data = dvc.api.read("data/new_data.csv", encoding='utf-8', repo=url)
+df = pd.read_csv(io.StringIO(data))
+
+# Convert boolean columns to 0 and 1
+df['Free'] = df['Free'].replace({True: 1, False: 0})
+df['Ad Supported'] = df['Ad Supported'].replace({True: 1, False: 0})
+df['In App Purchases'] = df['In App Purchases'].replace({True: 1, False: 0})
+df['Editors Choice'] = df['Editors Choice'].replace({True: 1, False: 0})
+
+# Convert categorical variables into dummy variables
+df = pd.get_dummies(df, columns=['Category', 'Content Rating'])
+
+# Feature Scaling
+scaler = StandardScaler()
+
+# Selecting numerical columns for scaling
+numerical_columns = ['Rating', 'Rating Count', 'Installs', 'Price', 'Size_M', 'Released_Year', 'Released_Month', 'Days_Between']
+df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+
+
+X = df[df.columns.difference(['Daily_Avg_Installs'])]
+y = df.Daily_Avg_Installs
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state = 42)
+
+# ======================== Model Training ====================================
+
+# model = SGDRegressor(max_iter=1000, tol=1e-3)
+# model.partial_fit(X_train, y_train)
+
+# polynomial = PolynomialFeatures(degree=2, include_bias= False, interaction_only = False)
+# X_train_poly = polynomial.fit_transform(X_train)
+# X_test_poly = polynomial.transform(X_test)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+rmse = root_mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("\nrmse:",rmse)
+print("r2:",r2)
+
+# filepath = Path('data/model_data.csv')
+# filepath.parent.mkdir(parents=True, exist_ok=True)
+# df.to_csv(filepath, index=False)
---
+++ version_new_data.py
@@ -0,0 +1,28 @@
+import subprocess
+import argparse
+
+# Parse CLI arguments
+parser = argparse.ArgumentParser(description="Run DVC and Git commands.")
+parser.add_argument("commit_message", type=str, help="Commit message for Git")
+args = parser.parse_args()
+
+# Define files paths
+data_file = "data/new_data.csv"
+dvc_file = "data/new_data.csv.dvc"
+gitignore_file = "data/.gitignore"
+
+commit_message = args.commit_message
+
+# Run DVC and Git commands
+subprocess.run(["dvc", "add", data_file], check=True)
+subprocess.run(["git", "add", dvc_file, gitignore_file, 'run_versioning.ps1', 'version_new_data.py',
+                'generate_data.py', 'model_training.py', 'README.md', 'images/', '.dvc/config'], check=True)
+
+subprocess.run(["git", "commit", "-m", commit_message], check=True)
+subprocess.run(["dvc", "push"], check=True)
+subprocess.run(["git", "push"], check=True)
+
+# If everything above worked without issues, the training will start
+# I will try to figure out a way better than this today
+print("\n======== Online Training triggered ===================\n")
+subprocess.run(["python", "online_train.py"], check=True)
