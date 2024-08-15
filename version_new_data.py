b7d9d312808efb94e9151a0060428239844e2d48
mostafa fallaha <moostafafallaha@gmail.com>
2024-08-15 20:49:46 +0300

    Add 4k

--- version_new_data.py
+++ version_new_data.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import check_drifting
 
 # Getting the commit message from the cmd
 parser = argparse.ArgumentParser(description="Run DVC and Git commands.")
@@ -13,6 +14,8 @@ gitignore_file = "data/.gitignore"
 
 commit_message = args.commit_message
 
+results = check_drifting.check_drift_all_features()
+
 # Run DVC and Git commands
 subprocess.run(["dvc", "add", data_file], check=True)
 subprocess.run(["git", "add", dvc_file, gitignore_file, 'run_versioning.ps1', 'version_new_data.py',
@@ -24,5 +27,9 @@ subprocess.run(["git", "push"], check=True)
 
 # If everything above worked without issues, the training will start
 # I will try to figure out a way better than this
-print("\n=================== Training triggered ===================\n")
-subprocess.run(["python", "model_training.py"], check=True)
+
+if results == 1:
+    print("\n============ Data drift detected, Training triggered ===================\n")
+    subprocess.run(["python", "model_training.py"], check=True)
+else:
+    print("\n=================== No Data drift ===================\n")
